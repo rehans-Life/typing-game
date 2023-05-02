a870a49f6a8a1349b8d9347c58c2adea10ef36d6
Your Name <you@example.com>
2023-05-02 14:57:53 -0700

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { faker } from '@faker-js/faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'typing-game';
+  randomText: string = faker.random.words(5);
+  text: string = '';
+
+  onInput(e: any): void {
+    this.text = e.target.value;
+    console.log(this.text);
+  }
 }
